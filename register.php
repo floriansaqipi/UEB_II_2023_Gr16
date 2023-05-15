5ffadaa070f47ccac1d5bf0fabb06322193b6856
erzamerovci <erzaamerovci@gmail.com>
2023-05-16 00:26:57 +0200

    added_email_verify_code

--- register.php
+++ register.php
@@ -1,25 +1,17 @@
-<?php include "includes/header.php"; ?>
+<?php $page = "register"; ?>
+<?php include 'includes/header.php'; ?>
+
+<?php include "includes/navabar.php"; ?>
 <link href="css/register.css" rel="stylesheet">
-<script src="js/register.js"></script>
+<!-- <script src="js/register.js"></script> -->
 
-    <div class="container-xxl bg-white p-0">
-        <!-- Spinner Start -->
-        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
-            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
-                <span class="sr-only">Loading...</span>
-            </div>
-        </div>
-        <!-- Spinner End -->
+    
 
-
-        <!-- Navbar Start -->
-        <?php include "includes/navabar.php"; ?>
-
-        <!-- Navbar End -->
-
+       
 
        
 		<div style="background-image:url('bg.png');"  class="container infinity-container">
+		
 		<div class="row">
 			<div  class="col-md-1 infinity-left-space"></div>
 
@@ -30,7 +22,7 @@
 					<h4>Create an account</h4>
 				</div>
 				<!-- Form -->
-				<form id="myForm" action="register.php" method="POST" class="px-6" autocomplete="on">
+				<form id="myForm" action="register-code.php" method="POST" class="px-6" autocomplete="on">
 					<!-- Input Box -->
 					<div class="form-input ">
 						<span><i class="fa fa-user"></i></span>
@@ -54,12 +46,12 @@
 					<div class="form-input">
 						<span><i class="fa fa-lock"></i></span>
 						<input id="password" type="password" name="password" placeholder="Password">
-						<div id="password-error" class="error-message"></div>
+						<!-- <div id="password-error" class="error-message"></div> -->
 					</div>
 					<div class="form-input">
 						<span><i class="fa fa-lock"></i></span>
 						<input id="confirm-password" type="password" name="confirm_password" placeholder="Confirm Password">
-						<div id="confirm-password-error" class="error-message"></div>
+						<!-- <div id="confirm-password-error" class="error-message"></div> -->
 					</div>
 
 					<div class="form-input">
@@ -79,9 +71,9 @@
 
 
 					<!-- Register Button -->
-					<div class="mb-3">
-						<button value="Register" type="submit" class="btn btn-block">Register</button>
-					</div>
+					<div class="col-12 ">
+					<button name="register" class="btn btn-primary" type="submit">Submit form</button>
+				</div>
 					<div class="text-center mb-5" style="color: #777;">Already have an account? 
 						<a class="login-link" href="login.php">Login here</a>
 			       	</div>
